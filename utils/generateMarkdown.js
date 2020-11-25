e3aa85233a9672ac865779df751397af07cef221
Star-Gibson <sgibsoncoding@gmail.com>
2020-11-25 18:47:33 -0500

    Took out most recent changes to markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,7 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-          # ${data.description}
-          # ${data.installation}
-          # ${data.usage}
-          # ${data.credits}
-          # ${data.license}
+          
 `;
 }
 
