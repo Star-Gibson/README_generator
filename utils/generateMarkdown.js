70791ae37c593559c428581393923555c4b6a958
Star-Gibson <sgibsoncoding@gmail.com>
2020-11-25 18:56:51 -0500

    Formed functional ReadMe contents in generateMarkdown.js + passed data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-          
+  return `
+  # Project Title : ${data.title}
+
+  ## Project Description: 
+  ${data.description}
+
+  ##Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+     
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ## License
+  ${data.license}
 `;
 }
 
