7b2d08c4ca6e6a89c8be6c395bec1a273cdf29fe
Star-Gibson <sgibsoncoding@gmail.com>
2020-11-25 18:20:55 -0500

    Added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-// array of questions for user
-const questions = [
+//Requiring inquirer and file system
+const inquirer = require('inquirer'); 
+const fs = require('fs');
 
+
+// array of questions for user
+const questions = [ 
+    {   type: 'input',
+        message: 'What is the name of your application?',
+        name: 'title',
+    },
+   
+    {   type: 'input',
+        message: 'How would you describe your application?',
+        name: 'description',
+    },
+   
+    {   type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },
+   
+    {   type: 'input',
+        message: 'Provide instructions and examples of use of your application.',
+        name: 'usage',
+    },
+   
+    {   type: 'confirm',
+        message: 'Were there any outside contributions or collaborators to your application?',
+        name: 'credits',
+
+    },
+    {   type: 'checkbox',
+        message: 'Which license would you like to include on your readme?',
+        name: 'license',
+        choices: ['MIT', 'Apache License 2.0', 'Boost Software', 'GNU AGPLv3']
+    },
+   
 ];
+console.log(questions) // Check for Questions - Take out upon completion
 
 // function to write README file
 function writeToFile(fileName, data) {
